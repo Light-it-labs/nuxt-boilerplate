deae5827a8e9c0c17875b242d2e5d0cba869b143
Juan <juan@lightit.io>
2020-01-07 15:34:58 -0300

    Add forgot password reset form

--- pages/login.vue
+++ pages/login.vue
@@ -25,15 +25,12 @@
 
     methods: {
       login() {
-        this.$nuxt.$loading.start();
         this.$auth.loginWith('local', {
           data: {
             email: this.form.email,
             password: this.form.password
           },
-        }).then(() => {
-          this.$nuxt.$loading.finish();
-        });
+        }).then(() => { });
       }
     },
   }
